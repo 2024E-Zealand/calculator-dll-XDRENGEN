5dd92aaec6efd15d67601ff478a34fadd9bed9a0
Christian Lund Nielsen <chn002@edu.zealand.dk>
2024-09-16 14:16:39 +0200

    +

---
+++ CalculatorClassLibrary/CalculatorAdd.cs
@@ -0,0 +1,21 @@
+﻿namespace CalculatorClassLibrary
+{
+    /// <summary>
+    /// Metode til at starte programmet.
+    /// Metoden bruges til en meget simpel regnemaskine,
+    /// som kan udføre +, -, * og / baseret på hvad brugeren vælger.
+    /// </summary>
+    public class CalculatorAdd
+    {
+        /// <summary>
+        /// Lægger to tal sammen 
+        /// </summary>
+        /// <param name="a">Det første tal som skal lægges sammen</param>
+        /// <param name="b">Det andet tal som skal lægges sammen</param>
+        /// <returns>Returnere summen af de to tal som er lagt sammen</returns>
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ CalculatorClassLibraryTests/CalculatorTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorClassLibrary.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void AddTest_PositivTal()
+        {
+            // Arrange
+            CalculatorAdd calculator = new CalculatorAdd();
+            double a = 10;
+            double b = 4;
+            double expected = 14;
+
+
+            // Act
+            double result = calculator.Add(a, b);
+
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+
+        [TestMethod()]
+        public void AddTest_NegativTest()
+        {
+            // Arrange
+            CalculatorAdd calculator = new CalculatorAdd();
+            double a = -10;
+            double b = -4;
+            double expected = -14;
+
+
+            // Act
+            double result = calculator.Add(a, b);
+
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+
+        [TestMethod()]
+        public void AddTest_NulTest()
+        {
+            // Arrange
+            CalculatorAdd calculator = new CalculatorAdd();
+            double a = 0;
+            double b = 0;
+            double expected = 0;
+
+
+            // Act
+            double result = calculator.Add(a, b);
+
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
